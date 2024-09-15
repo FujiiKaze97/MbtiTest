0947916c642df888c7b70b7c27b2af844214614b
hsw <hongsw6934@gmail.com>
2024-09-15 23:53:11 +0900

    update router setting , home.jsx setting

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,42 +1,31 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-// Props 로 꼭 children 만 받을 필요는 없답니다. 
-const Layout = ({ children }) => {
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      
-    }, []);
-  
-    const handleLogout = () => {
-  
-    };
-  
-    return (
-      <div>
-        <header>
-          <nav>
-            <Link to="/">
-              홈
-            </Link>
-            <div className="space-x-4">
-              {user ? (
-                <>
-                  <button onClick={handleLogout}>
-                    로그아웃
-                  </button>
-                </>
-              ) : (
-                <Link to="/login">
-                  로그인
-                </Link>
-              )}
-            </div>
-          </nav>
-        </header>
-        <main className="container mx-auto pt-10 main">{children}</main>
-      </div>
-    );
-  };
-  
-  export default Layout;
-  
+// Props 로 꼭 children 만 받을 필요는 없답니다.
+const Layout = ({ user, children }) => {
+  const navigate = useNavigate();
+  useEffect(() => {}, []);
+  const handleLogout = () => {};
+
+  return (
+    <div>
+      <header class="py-3 px-3">
+        <nav class="flex justify-between">
+          <Link to="/">홈</Link>
+          <div className="space-x-4">
+            {user ? (
+              <>
+                <button onClick={handleLogout}>로그아웃</button>
+              </>
+            ) : (
+              <Link to="/login">로그인</Link>
+            )}
+          </div>
+        </nav>
+      </header>
+      <main className="container mx-auto pt-10 main">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
